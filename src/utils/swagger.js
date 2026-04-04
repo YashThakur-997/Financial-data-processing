a410fda7509be3212b59281296c1b866714beda8
Yash Thakur <yashthakur99755@gmail.com>
2026-04-04 22:07:56 +0530

    fix: update server URL in Swagger setup to use production endpoint

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://financial-data-processing-ekfa.onrender.com',
       },
     ],
   },
